f51794178916891f2bee13efb25a1e512347c76f
Christos Mantsos <xristosman18@gmail.com>
2025-03-07 14:06:16 +0200

    backend: catch duplicate users

--- server/app.js
+++ server/app.js
@@ -1,48 +1,55 @@
-const express = require('express');
-const models = require('./models');
-const { body, validationResult } = require('express-validator');
-const app = express()
+const express = require("express");
+const models = require("./models");
+const { Op } = require("sequelize");
+const { body, validationResult } = require("express-validator");
+const app = express();
 
 // JSON parser
-app.use(express.json())// this is a middleware so the request will be parsed as JSON
+app.use(express.json()); // this is a middleware so the request will be parsed as JSON
 
 const registerValidator = [
-    body('username', 'username cannot be emty!').not().isEmpty(),
-    body('password', 'password cannot be emty!').not().isEmpty()
-]
+  body("username", "username cannot be emty!").not().isEmpty(),
+  body("password", "password cannot be emty!").not().isEmpty(),
+];
 
-app.post('/register', registerValidator, (req, res) => {
+app.post("/register", registerValidator, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const mes = errors
+      .array()
+      .map((e) => e.msg)
+      .join(", ");
+    return res.status(422).json({ success: false, errors: mes });
+  }
 
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        const mes = errors.array().map(e => e.msg).join(', ')
-        return res.status(422).json({ success: false , errors: mes })
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await models.User.findOne({
+      where: {
+        username: { [Op.iLike]: username },
+      },
+    });
+
+    if (existingUser) {
+      return res.json({ success: false, errors: "Username already exists" });
     }
-    
 
-    const {username, password} = req.body
-    
     //create a new user
 
     const newUser = models.User.create({
-        username: username,
-        password: password
-    })
+      username: username,
+      password: password,
+    });
 
-    //validate the request
-
-
-
-    res.status(201).json({ success: true})
-    
-
-
-
-
-
-})
+    res.status(201).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, errors: "Internal server error" });
+  }
+});
 
 //start the server
 app.listen(8080, () => {
-    console.log('Server is running at http://localhost:8080 ')
-})
+  console.log("Server is running at http://localhost:8080 ");
+});
