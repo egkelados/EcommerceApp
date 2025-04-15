2cd56f36f777c24193c080e131d670bba225ac20
Christos Mantsos <xristosman18@gmail.com>
2025-04-15 22:38:35 +0300

    backend: add migrations for order and orderItem

---
+++ iOS/SmartShop/random.swift
@@ -0,0 +1,18 @@
+//
+//  random.swift
+//  SmartShop
+//
+//  Created by Xristos Mantsos on 14/4/25.
+//
+
+import SwiftUI
+
+struct random: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    random()
+}
